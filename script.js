326cde12bebc71afe1b4be148359270c9013e61d
“Xhesika” <xhesikam2184@tywls.org>
2023-03-27 09:58:30 -0400

    exercise working

--- script.js
+++ script.js
@@ -19,20 +19,20 @@ fetch( `${quoteApi}` , quoteAuth )
 
 
 // Exercise Fetch
-let exercise //This was an example, the real var will depend on user input//= "volleyball";
-const exerciseAuth = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b7033af962msh5e6aefcc11f4288p181037jsn8d2e49ec14db',
-		'X-RapidAPI-Host': 'calories-burned-by-api-ninjas.p.rapidapi.com'
-	}
-};
-const exerciseApi = 'https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned?activity=skiing'
+// let exercise //This was an example, the real var will depend on user input//= "volleyball";
+// const exerciseAuth = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'b7033af962msh5e6aefcc11f4288p181037jsn8d2e49ec14db',
+// 		'X-RapidAPI-Host': 'calories-burned-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+// const exerciseApi = 'https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned?activity=skiing'
 
-fetch(`${exerciseApi}${exercise}`, exerciseAuth)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch(`${exerciseApi}${exercise}`, exerciseAuth)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
 /*
     Will return sum like this. We can use this to divide the calorie/h count by duration (for calories per minute) and then multiply it by total minutes logged by user.
